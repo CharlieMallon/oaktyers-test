09129187fd14c2130311bee70ba20642b2f978cb
Charlotte Mallon <charlie@Charlottes-MacBook-Pro.local>
2023-01-09 20:16:28 +0000

    basic results card

---
+++ src/components/ResultsCard/ResultsCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./ResultCard.module.css";
+
+const ResultCard = () => {
+    return (
+        <div className={styles.main}>
+            <h5>results go here</h5>
+        </div>
+    );
+};
+
+export default ResultCard;
