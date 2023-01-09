2b699b178bfcee94345c75cd7ddde8083ff7a12d
Charlotte Mallon <charlie@Charlottes-MacBook-Pro.local>
2023-01-09 20:24:23 +0000

    structure for searchPage added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
+import SearchPage from './pages/SearchPage/searchPage';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <SearchPage/>
     </div>
   );
 }
---
+++ src/pages/SearchPage/searchPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styles from "./SearchPage.module.css"
+
+const SearchPage = () => {
+    // boxes for ?page and ?per_page go here
+    // result cards go here
+    return (
+        <div className = {styles.main}>
+            <h4>results</h4>
+        </div>
+    )
+}
+
+export default SearchPage
