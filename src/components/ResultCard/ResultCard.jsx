fc624e0bb5eb462c22181d4998109ae31df99d7a
Charlotte Mallon <charlie@Charlottes-MacBook-Pro.local>
2023-01-10 03:13:33 +0000

    card styling

--- src/components/ResultCard/ResultCard.jsx
+++ src/components/ResultCard/ResultCard.jsx
@@ -5,7 +5,7 @@ const ResultCard = ({ cardData }) => {
       return (
           <div className={styles.main}>
               <h2 className={styles.name}>{cardData.name}</h2>
-              <p>{cardData.tagline}</p>
+              <p className={styles.tagline}>{cardData.tagline}</p>
             <div className={styles.imageContainer}>
               <img className={styles.image} src={cardData.image_url} alt={cardData.name} />
             </div>
